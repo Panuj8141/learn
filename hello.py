677211e1ab972a467ff7460879b456b01a21fc72
Anuj pal <panuj8141@gmail.com>
2024-12-31 23:53:15 +0530

    changning

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello World!!")
+def greet(name):
+   print("hello", name)
+
+name = input("whats your name?")
+greet(name)
+age = input("whats your age?")
+print("ohh i see so you are", age , "years old")
+again = input("sorry!... whats your name again?")
+if again == "anuj" or again =="Anuj":
+	print("you are a good guy")
+else:
+	print("gay!!!")
